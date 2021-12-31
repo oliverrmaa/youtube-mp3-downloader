5ab38b03582ffc225c3ba4e2f49aff7cdb2661f1
Oliver Ma <oliver.zheyi.ma@gmail.com>
2021-12-31 13:31:37 -0800

    first commit

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
 # youtube-mp3-downloader
 
-This repo utilizes the `youtube_dl` library to download songs from youtube in bulk. To utilize this program open terminal and run the following command:
+This repo utilizes the `youtube_dl` library to download songs from youtube in
+bulk. Please install dependencies using:
+
+```
+cd youtube-mp3-downloader
+pip install -r requirements.txt
+```
+
+To utilize this program open the command line and run the following
+commands:
 
 ```
 cd youtube-mp3-downloader
 python downloader.py
 ```
 
-The first run will create the necessary `Songs` directory and subsequent downloaded songs will be placed here. To add a list of songs to be downloaded, simply go to the newly created `queue.txt` file in `Songs` and paste the necessary youtube links in. Please only place one link per line and make sure to not have any extra spaces on the line. 
+The first run will create the necessary `Songs` directory and subsequent
+downloaded songs will be placed here. To add a list of songs to be downloaded,
+simply go to the newly created `queue.txt` file and paste the necessary youtube
+links in. Please only place one link per line and make sure to not have any
+extra spaces on the line.
+
+IF you are a mac user, it is possible further dependencies may be needed in
+which case, please use `brew install ffmpeg`.
--- downloader.py
+++ downloader.py
@@ -15,33 +15,66 @@ download_options = {
     }],
 }
 
+def create_songs_dir(songs_dir):
+    """Creates the songs directory if it does not exist already."""
+    # If song directory does not exist, create song directory
+    if not os.path.exists(songs_dir):
+        os.mkdir(songs_dir)
+        os.chdir(songs_dir)
+    else:
+        os.chdir(songs_dir)
 
-# If song directory does not exist, create song directory
-if not os.path.exists('Songs'):
-    os.mkdir('Songs')
-    os.chdir('Songs')
-else:
-    os.chdir('Songs')
+def create_queue(queue_list_path):
+    """
+    Generate queue_list with songs or creates queue.txt in songs directory if
+    it does not exist already.
+    """
+    # If song download queue list does not exist, create queue list
+    if not os.path.exists(queue_list_path):
+        print("First run, creating empty queue list")
+        open(queue_list_path, 'a').close()
+        queue_list = list()
+    else:
+        with open(queue_list_path) as q:
+            queue_list = q.readlines()
 
-# If song download queue list does not exist, create queue list
-if not os.path.exists('./queue.txt'):
-    print("First run, creating empty queue list")
-    open('./queue.txt', 'a').close()
-    queue_list = list()
-else:
-    with open('./queue.txt') as q:
-        queue_list = q.readlines()
+    return (queue_list)
 
-# Download songs in queue
-if not len(queue_list)==0:
-    for idx, song in enumerate(queue_list):
-        print("Now downloading song {} / {}".format(idx+1, len(queue_list)))
-        with youtube_dl.YoutubeDL(download_options) as ydl:
-            ydl.download([song])
-else:
-    print("No songs in the queue")
+def open_queue(queue_list_path):
+    """Opens a queue .txt file for the user"""
+    os.system("open " + queue_list_path)
 
-# Clear the queue list for use on the next run
-print("Now erasing the queue list for reuse on next run")
-q = open('./queue.txt', 'w')
-q.close()
+def download_songs(queue_list, download_options=download_options):
+    """Takes in a queue_list as input and downloads each song on the list."""
+    # Download songs in queue
+    if not len(queue_list)==0:
+        for idx, song in enumerate(queue_list):
+            print("Now downloading song {} / {}".format(idx+1, len(queue_list)))
+            with youtube_dl.YoutubeDL(download_options) as ydl:
+                ydl.download([song])
+    else:
+        print("No songs in the queue")
+
+
+if __name__ == '__main__':
+
+    # Create necessary paths
+    cwd = os.getcwd()
+    songs_dir = cwd + '/Songs/'
+    queue_list_path = cwd + '/queue.txt'
+
+    if argv[1] == "open-queue":
+        open_queue(queue_list_path)
+    else:
+        # Run functions
+        create_songs_dir(songs_dir)
+        queue_list = create_queue(queue_list_path)
+        download_songs(queue_list)
+        # Clear the queue list for use on the next run
+        print("Now erasing the queue list for reuse on next run")
+        q = open(queue_list_path, 'w')
+        q.close()
+
+    # Return to the starting directory
+    path_parent = os.path.dirname(os.getcwd())
+    os.chdir(path_parent)
